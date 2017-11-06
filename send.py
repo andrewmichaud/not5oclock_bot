c2b180673b617e0f1815c814c250800a74b1bb45
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-11-06 00:42:26 -0800

    use botskeleton

--- send.py
+++ send.py
@@ -1,33 +1,13 @@
+"""Send file for not5oclock_bot"""
+
 import argparse
-import os
+from os import path
 
-import tweepy
-
-
-def auth_and_get_api():
-    """Authenticate with twitter and get access to API."""
-    # auth auth auth auth
-    SECRETS_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "SECRETS")
-
-    with open(os.path.join(SECRETS_DIR, "CONSUMER_KEY")) as f:
-        CONSUMER_KEY = f.read().strip()
-
-    with open(os.path.join(SECRETS_DIR, "CONSUMER_SECRET")) as f:
-        CONSUMER_SECRET = f.read().strip()
-
-    with open(os.path.join(SECRETS_DIR, "ACCESS_TOKEN")) as f:
-        ACCESS_TOKEN = f.read().strip()
-
-    with open(os.path.join(SECRETS_DIR, "ACCESS_SECRET")) as f:
-        ACCESS_SECRET = f.read().strip()
-
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-
-    return tweepy.API(auth)
+import botskeleton
 
 if __name__ == "__main__":
-    api = auth_and_get_api()
+    SECRETS_DIR = path.join(path.abspath(path.dirname(__file__)), "SECRETS")
+    botskeleton = botskeleton.BotSkeleton(SECRETS_DIR, bot_name="isthisska_bot")
 
     # Get arg.
     # Could probably do it more simply - this is doing it properly but half-assed.
@@ -37,5 +17,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     tweet = vars(args)["text"]
 
-    print("Tweeting '{}'.".format(tweet))
-    api.update_status(tweet)
+    botskeleton.send(tweet)
